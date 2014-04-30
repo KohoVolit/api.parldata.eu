712aafa90f61dcd5aaa2560052f9d751e915b984
Jaroslav Semančík <jaroslav_semancik@yahoo.com>
2014-04-30 22:25:55 +0200

    added SSL support to API client module

--- client/vpapi.py
+++ client/vpapi.py
@@ -8,31 +8,44 @@ Contains functions to make API requests easily.
 
 __all__ = ['parliament', 'authorization', 'get', 'post', 'put', 'patch', 'delete']
 
-SERVER_NAME = 'http://127.0.0.1:5000'
+SERVER_NAME = 'api.parldata.eu'
+SERVER_CERT = 'server_cert.pem'
 PARLIAMENT = ''
 PAYLOAD_HEADERS = {
 	'Content-Type': 'application/json',
 }
 
-def _endpoint(resource):
-	# Returns URL of the given resource.
-	if PARLIAMENT:
-		return '%s/%s/%s' % (SERVER_NAME, PARLIAMENT, resource)
+def _endpoint(resource, method):
+	"""Returns URL of the given resource and method.
+	http:// is used for GET method while https:// for the others.
+	http:// is used for all methods on localhost.
+	"""
+	if method=='GET' or SERVER_NAME.startswith('localhost:') or SERVER_NAME.startswith('127.0.0.1:'):
+		protocol = 'http'
 	else:
-		return '%s/%s' % (SERVER_NAME, resource)
+		protocol = 'https'
+	if PARLIAMENT:
+		url = '%s://%s/%s/%s' % (protocol, SERVER_NAME, PARLIAMENT, resource)
+	else:
+		url = '%s://%s/%s' % (protocol, SERVER_NAME, resource)
+	return url
+
 
 def _jsonify_dict_values(params):
-	# Returns `params` dictionary with all values of type dictionary or list
-	# serialized by JSON. This is necessary for _request_ library to pass
-	# parameters in the query string correctly.
+	"""Returns `params` dictionary with all values of type dictionary
+	or list serialized to JSON. This is necessary for _requests_
+	library to pass parameters in the query string correctly.
+	"""
 	return { k: json.dumps(v)	if isinstance(v, dict) or isinstance(v, list) else v
 		for k, v in params.items()
 	}
+
 
 def parliament(parl):
 	"""Sets the parliament the following requests will be sent to."""
 	global PARLIAMENT
 	PARLIAMENT = parl
+
 
 def authorize(username, password):
 	"""Sets API username and password for the following data modifying
@@ -41,21 +54,26 @@ def authorize(username, password):
 	s = '%s:%s' % (username, password)
 	PAYLOAD_HEADERS['Authorization'] = b'Basic ' + base64.b64encode(s.encode('ascii'))
 
+
 def deauthorize():
 	"""Unsets API username and password - the following data modifying
 	requests will be anonymous.
 	"""
 	PAYLOAD_HEADERS.pop('Authorization', None)
 
+
 def get(resource, **kwargs):
 	"""Makes a GET (read) request to the API.
 	Lookup parameters are specified as keyword arguments.
 	"""
 	resp = requests.get(
-		_endpoint(resource),
-		params=_jsonify_dict_values(kwargs))
+		_endpoint(resource, 'GET'),
+		params=_jsonify_dict_values(kwargs),
+		verify=SERVER_CERT
+	)
 	resp.raise_for_status()
 	return resp.json()
+
 
 def post(resource, data, **kwargs):
 	"""Makes a POST (create) request to the API.
@@ -63,12 +81,15 @@ def post(resource, data, **kwargs):
 	and eventual parameters may be specified as keyword arguments.
 	"""
 	resp = requests.post(
-		_endpoint(resource),
+		_endpoint(resource, 'POST'),
 		params=_jsonify_dict_values(kwargs),
 		data=json.dumps(data),
-		headers=PAYLOAD_HEADERS)
+		headers=PAYLOAD_HEADERS,
+		verify=SERVER_CERT
+	)
 	resp.raise_for_status()
 	return resp.json()
+
 
 def put(resource, data, **kwargs):
 	"""Makes a PUT (replace) request to the API.
@@ -76,13 +97,15 @@ def put(resource, data, **kwargs):
 	eventual parameters may be specified as keyword arguments.
 	"""
 	resp = requests.put(
-		_endpoint(resource),
+		_endpoint(resource, 'PUT'),
 		params=_jsonify_dict_values(kwargs),
 		data=json.dumps(data),
-		headers=PAYLOAD_HEADERS
+		headers=PAYLOAD_HEADERS,
+		verify=SERVER_CERT
 	)
 	resp.raise_for_status()
 	return resp.json()
+
 
 def patch(resource, data, **kwargs):
 	"""Makes a PATCH (update) request to the API.
@@ -90,22 +113,22 @@ def patch(resource, data, **kwargs):
 	eventual parameters may be specified as keyword arguments.
 	"""
 	resp = requests.patch(
-		_endpoint(resource),
+		_endpoint(resource, 'PATCH'),
 		params=_jsonify_dict_values(kwargs),
 		data=json.dumps(data),
-		headers=PAYLOAD_HEADERS
-	)
+		headers=PAYLOAD_HEADERS,
+		verify=SERVER_CERT
+)
 	resp.raise_for_status()
 	return resp.json()
 
-def delete(resource, **kwargs):
-	"""Makes a DELETE request to the API.
-	Lookup parameters are specified as keyword arguments.
-	"""
+
+def delete(resource):
+	"""Makes a DELETE request to the API."""
 	resp = requests.delete(
-		_endpoint(resource),
-		params=_jsonify_dict_values(kwargs),
-		headers=PAYLOAD_HEADERS
+		_endpoint(resource, 'DELETE'),
+		headers=PAYLOAD_HEADERS,
+		verify=SERVER_CERT
 	)
 	resp.raise_for_status()
 	return resp.json()
